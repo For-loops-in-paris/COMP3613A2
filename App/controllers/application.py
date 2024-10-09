c9fce3be3226fb7db4709321103740b6b5560ebe
Mark1Lessey <mark.lessey1@my.uwi.edu>
2024-10-08 23:24:26 -0400

    Refactoring code

--- App/controllers/application.py
+++ App/controllers/application.py
@@ -9,9 +9,6 @@ def create_application(job_id,applicant_id):
     if Application.query.filter_by(job_id=job_id,applicant_id=applicant_id).first() != None:
         return False
         
-    if not Applicant.query.get(applicant_id):
-        
-        return False
     try:
         application = Application(job_id,applicant_id)
         db.session.add(application)
