4bebaa17ab5a8262b205bd9c078eda78974b806f
Mark1Lessey <mark.lessey1@my.uwi.edu>
2024-10-08 22:43:41 -0400

    Applicant view created

---
+++ App/views/applicant.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, render_template, jsonify, request, flash, send_from_directory, flash, redirect, url_for
+from flask_login import login_user, login_manager, logout_user, LoginManager, current_user
+from flask_jwt_extended import jwt_required, current_user, unset_jwt_cookies, set_access_cookies
+from App.controllers.auth import applicant_required
+
+from App.controllers import(
+    create_application
+)
+
+applicant_views = Blueprint('applicant_views', __name__, template_folder='../templates')
+
+@applicant_views.route('/create_application', methods=['POST'])
+def create_applicant():
+    data=request.json
+    application = create_application(data['job_id'], data['applicant_id'])
+    if application:
+        return jsonify({"message": "Application made."}), 201
+    else:
+        return jsonify({"message":"Application failed."}), 400
