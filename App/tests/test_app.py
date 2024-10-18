7bad095c001b387efd8975cd9364ee17dfd35a21
AnthonyCharles13 <orlandosahadeo13@gmail.com>
2024-10-18 17:24:35 -0400

    updated integration tests

--- App/tests/test_app.py
+++ App/tests/test_app.py
@@ -18,11 +18,7 @@ from App.controllers import (
     view_applicants_json,
     apply_to_job,
     withdraw_application,
-    get_all_users_json,
-    login,
-    get_user,
-    get_user_by_username,
-    update_user
+    get_user_by_username
 )
 
 
@@ -99,22 +95,22 @@ class UsersIntegrationTests(unittest.TestCase):
     def test_01_create_user(self): #ID IS 1
         user = create_user("ross", "rosspass", "ross@mail.com")
         new_user = get_user_by_username("ross")
-        assert new_user.username == "ross"
+        assert new_user.username == "ross" and new_user.email_address == "ross@mail.com"
         
     def test_02_create_admin(self): # ID IS 2
         admin = create_admin("bob", "bobpass", "bob@mail.com")
         new_admin = get_user_by_username("bob")
-        assert new_admin.username == "bob"
+        assert new_admin.username == "bob" and new_admin.email_address == "bob@mail.com"
     
     def test_03_create_company(self): # ID IS 1
         create_company('Google','https://cdn2.hubspot.net/hubfs/53/image8-2.jpg')
         new_company = get_company(1)
-        assert new_company.company_name == "Google"
+        assert new_company.company_name == "Google" and new_company.company_logo == "https://cdn2.hubspot.net/hubfs/53/image8-2.jpg"
 
     def test_04_create_recruiter(self): # ID IS 3
         create_recruiter("rick", "rickpass","rick@mail.com")
         recruiter = get_user_by_username('rick')
-        assert recruiter.username == "rick"
+        assert recruiter.username == "rick" and recruiter.email_address == "rick@mail.com"
         
     def test_05_assign_recruiter(self):
         admin = get_user_by_username("bob")
@@ -124,15 +120,15 @@ class UsersIntegrationTests(unittest.TestCase):
         assert recruiter.company_id == 1
         
     def test_06_create_applicant(self): # ID IS 4
-        create_applicant("BruceWayne", "brucepass", "Bruce", "Wayne", 456-9535, "bruce@wayne.com")
+        create_applicant("BruceWayne", "brucepass", "Bruce", "Wayne", "456-9535", "bruce@wayne.com")
         applicant = get_user_by_username("BruceWayne")
-        assert applicant.username == "BruceWayne"
+        assert applicant.username == "BruceWayne" and applicant.first_name == "Bruce" and applicant.last_name == "Wayne" and applicant.phone_number == "456-9535" and applicant.email_address == "bruce@wayne.com"
 
     def test_07_create_job(self): # ID IS 1
         recruiter = get_user_by_username('rick')
         create_job(recruiter.id, "JuniorDev", "Develops things juniorly", 999)
         new_job = get_job(1)
-        assert new_job.position == "JuniorDev"
+        assert new_job.recruiter_id == recruiter.id and new_job.position == "JuniorDev" and new_job.description == "Develops things juniorly" and new_job.salary == 999
         
     def test_08_view_jobs_json(self):
         recruiter = get_user_by_username('rick')
@@ -144,24 +140,28 @@ class UsersIntegrationTests(unittest.TestCase):
         ]
         self.assertListEqual(expected_jobs, jobs)
     
-    def _test_09_apply_to_job(self):
-        new_job = get_job(1)
+    def test_09_apply_to_job(self):
+        job = get_job(1)
         applicant = get_user_by_username("BruceWayne")
-        apply_msg = apply_to_job(new_job.id, applicant.id)
-        print(apply_msg)
-        assert applicant.id == 9
+        apply_msg = apply_to_job(job.id, applicant.id)
+        assert apply_msg == "Application successful"
         
     def test_10_view_applicants_json(self):
-        new_job = get_job(1)
-        applicants_json = view_applicants_json(new_job.id)
+        job = get_job(1)
+        applicants_json = view_applicants_json(job.id)
         expected_applicants = [{"id": 4, "username": "BruceWayne"}]
         self.assertListEqual(expected_applicants, applicants_json)
         
-    #def test_11_withdraw_application(self):
-     #   new_job = get_job(1)
-      #  applicant = get_user_by_username("BruceWayne")
-       # withdraw_result = withdraw_application(1, 4)
-        #assert withdraw_result == True
+    def test_11_withdraw_application(self):
+        job = get_job(1)
+        applicant = get_user_by_username("BruceWayne")
+        #create_applicant("Tommy", "toepass", "Tomla", "stNae", "456-9535", "tom@wayne.com")
+        #secapp = get_user_by_username("Tommy")
+        #apply_to_job(job.id, secapp.id)
+        #print(view_applicants_json(job.id))
+        withdraw_result = withdraw_application(job.id, applicant.id)
+        #print(view_applicants_json(job.id))
+        assert withdraw_result == True
         
     def test_12_job_get_json(self):
         new_job = get_job(1)
